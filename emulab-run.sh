bfcd0526a9cbb9219276002ba3b2fd770647a066
Yosub Shin <yosub@yosubshin.com>
2015-03-27 23:51:58 -0500

    update

--- emulab-run.sh
+++ emulab-run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Copy ssh keys and public keys so that root user can freely login to other nodes without prompted for password
+
+if [ "`sudo cat /root/.ssh/authorized_keys | wc -l`" -lt "10" ]; then
 parallel-ssh -h /tmp/hosts 'sudo bash -c "cp /users/yossupp/.ssh/id_rsa /root/.ssh/;cat /users/yossupp/.ssh/authorized_keys >> /root/.ssh/authorized_keys"'
+fi
 
 sudo -u root bash <<EOF
 
