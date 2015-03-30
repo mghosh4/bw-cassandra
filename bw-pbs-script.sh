07faa3c155df2a5060646212684f816c7808fc99
Yosub Shin <yosub@yosubshin.com>
2015-03-30 14:47:35 -0500

    update

--- bw-pbs-script.sh
+++ bw-pbs-script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#PBS -j oe
 #PBS -l nodes=20:ppn=32:xe
 #PBS -l walltime=02:00:00
 #PBS -l gres=ccm
+#PBS -j oe
+#PBS -o /u/sciteam/shin1/scratch/$PBS_JOBID.out
+#PBS -m bea
+#PBS -M shin14@illinois.edu
 
-export APRUN_XFER_LIMITS=1 
-
+. /opt/modules/default/init/bash
 module load ccm
+
+export APRUN_XFER_LIMITS=1
 
 cd /u/sciteam/shin1/scratch/bw-cassandra
 ccmrun sh bw-ccmrun.sh
