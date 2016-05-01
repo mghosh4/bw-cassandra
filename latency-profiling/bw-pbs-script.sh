447c3447728bc3e7bf3691c7d695129c1223df95
Mainak Ghosh <ghosh1@h2ologin1.ncsa.illinois.edu>
2016-05-01 11:47:35 -0500

    Final Commit from BW allocation

--- latency-profiling/bw-pbs-script.sh
+++ latency-profiling/bw-pbs-script.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-#PBS -l nodes=100:ppn=32:xe
-#PBS -l walltime=01:00:00
-#PBS -l gres=ccm
+#PBS -l nodes=96:ppn=32:xe
+#PBS -l walltime=02:00:00
+#PBS -l geometry=4x4x3
 ##PBS -j oe
 ##PBS -o /u/sciteam/ghosh1/scratch/$PBS_JOBID.out
 #PBS -e /u/sciteam/ghosh1/scratch/log/$PBS_JOBID.err
 #PBS -o /u/sciteam/ghosh1/scratch/log/$PBS_JOBID.out
 #PBS -m bea
-#PBS -M ghosh14@illinois.edu
+#PBS -M ghosh1@illinois.edu
 
 
 source /opt/modules/default/init/bash
-module list
+echo $PBS_JOBID
+#module list
 
 #. /opt/modules/default/init/bash
-module load ccm
+#module load ccm
 
 export APRUN_XFER_LIMITS=1
 
 cd /projects/sciteam/jsb/ghosh1/bw-cassandra/latency-profiling
-ccmrun sh bw-ccmrun.sh
+aprun -n 96 -N 1 testlatency.sh
--- latency-profiling/testlatency.sh
+++ latency-profiling/testlatency.sh
@@ -5,7 +5,6 @@ NUM_PINGS=30
 
 filename=`hostname`
 latencyfilename="$filename"_latency.log
-trfilename="$filename"_tr.log
 
 nodenames=$(cat /projects/sciteam/jsb/ghosh1/result/hostlist | awk '{system("rca-helper -x "$1)}')
 
@@ -13,6 +12,5 @@ for name in $nodenames
 do
 	if [ `hostname` != $name ]; then
 		ping -i 0.2 -s 16384 -c $NUM_PINGS $name >> $resultpath/$latencyfilename
-		/usr/sbin/traceroute $name >> $resultpath/$trfilename
 	fi
 done
